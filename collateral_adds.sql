2a7b911f651724be802bf9fe56f1197d6539d8d2
Jules Dourlens <jdourlens@localhost.localdomain>
2020-10-03 11:46:31 +0200

    Added compound add/redeem

--- collateral_adds.sql
+++ collateral_adds.sql
@@ -19,3 +19,32 @@ FROM aave."LendingPool_evt_Deposit"
 
 
         --Compound
+SELECT evt_block_number AS block_number,
+       evt_block_time AS block_time,
+       c."underlying_token_address" AS asset_address,
+       CASE
+           WHEN t.symbol = 'WETH' THEN 'ETH'
+           ELSE t.symbol
+       END AS token_symbol,
+       t.decimals AS token_decimals,
+       "mintAmount"/10^t.decimals AS asset_amount,
+       "mintAmount"/10^t.decimals*p.price AS asset_amount_usd,
+       minter AS "user",
+       'Compound' AS "project",
+       '2' AS "version",
+       evt_tx_hash AS tx_hash
+FROM
+  (SELECT *
+   FROM compound_v2."cErc20_evt_Mint"
+   UNION SELECT *
+   FROM compound_v2."cEther_evt_Mint"
+   UNION SELECT *
+   FROM compound_v2."CErc20Delegator_evt_Mint") EVENTS
+LEFT JOIN compound.view_ctokens c ON events.contract_address = c.contract_address
+LEFT JOIN erc20.tokens t ON c.underlying_token_address = t.contract_address
+LEFT JOIN prices.usd p ON p.minute = date_trunc('minute', evt_block_time)
+AND p.contract_address = c.underlying_token_address
+
+
+
+-- MakerDAO
--- collateral_redeems.sql
+++ collateral_redeems.sql
@@ -18,3 +18,31 @@ FROM aave."LendingPool_evt_RedeemUnderlying"
         AND p."contract_address" = "_reserve" LIMIT
 
         --Compound
+
+        SELECT evt_block_number AS block_number,
+       evt_block_time AS block_time,
+       c."underlying_token_address" AS asset_address,
+       CASE
+           WHEN t.symbol = 'WETH' THEN 'ETH'
+           ELSE t.symbol
+       END AS token_symbol,
+       t.decimals AS token_decimals,
+       "redeemAmount"/10^t.decimals AS asset_amount,
+       "redeemAmount"/10^t.decimals*p.price AS asset_amount_usd,
+       redeemer AS "user",
+       'Compound' AS "project",
+       '2' AS "version",
+       evt_tx_hash AS tx_hash
+FROM
+  (SELECT *
+   FROM compound_v2."cErc20_evt_Redeem"
+   UNION SELECT *
+   FROM compound_v2."cEther_evt_Redeem"
+   UNION SELECT *
+   FROM compound_v2."CErc20Delegator_evt_Redeem") EVENTS
+LEFT JOIN compound.view_ctokens c ON events.contract_address = c.contract_address
+LEFT JOIN erc20.tokens t ON c.underlying_token_address = t.contract_address
+LEFT JOIN prices.usd p ON p.minute = date_trunc('minute', evt_block_time)
+AND p.contract_address = c.underlying_token_address
+
+-- MAkerDAO
